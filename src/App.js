c1c0c848029581a0e7b0365fd601b73b49659edb
tomdmitchell <thomasdavymitchell@gmail.com>
2019-07-10 21:32:06 +0100

    put all in one file and add delete state functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Axios from "axios";
-import MyRender from "./MyRender.js";
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
             isLoading: true,
-            data: null
+            data: []
         }
+        this.getChecked = this.getChecked.bind(this);
     }
 
-    getData() {
+    getChecked() {
+        var {data} = this.state;
+        console.log(this.state.data);
+        var imageCheckboxes = document.querySelectorAll('.imageCheckboxes');
+        var checkedVals = [];
+        imageCheckboxes.forEach(function(checkbox) {
+          if (checkbox.checked) {
+            checkedVals.push({imageId: checkbox.value});
+          }
+        })
+        Axios.post("http://localhost:3000/deleteimage", checkedVals, {
+        })
+        .then(res => {
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        var stateCopy = this.state.data;
+        for (var j = 0; j < checkedVals.length; j++) {
+            for (var i = 0; i < stateCopy.length; i++) {
+                if (stateCopy[i].imagePath === checkedVals[j].imageId) {
+                    stateCopy.splice(i,1);
+                    break;
+                }
+            }
+        }
+        this.setState({data: stateCopy});
+        console.log(this.state.data);
+    }
+
+
+     getData() {
         Axios.get('http://localhost:3000/images')
         .then(res => {
             var objArray = res.data;
+            console.log('looooooaddddded');
             this.setState({
                 data: objArray,
                 isLoading: false
@@ -34,11 +66,25 @@ class App extends React.Component {
         return (
             <div>
                {
-                   isLoading ? <div>Loading....</div> : <MyRender data= {this.state.data}/>
+                   isLoading ? <div>Loading....</div> :
+                   <div>
+                   {data.map(function(x) {
+                       return (
+                           <div key={x._id}>
+                               <img key ={x._id + "a"} style={{width:200, height:200}} src={`http://localhost:3000/${x.imagePath}`} />
+                               <input className='imageCheckboxes' type='checkbox' key={x._id} value={x.imagePath}></input>
+                           </div>
+                       )
+                   })}
+                   <button onClick={this.getChecked}>Remove elements</button>
+                   </div>
                }
             </div>
         )
     }
 }
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root')); 
