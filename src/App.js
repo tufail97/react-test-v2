ac2e1392e154a96806c0360192aa6053e79023aa
tomdmitchell <thomasdavymitchell@gmail.com>
2019-07-14 16:39:47 +0100

    remove @reach/router, add in react-router

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AdminPanel from "./admin-panel/AdminPanel.js";
 import FrontPage from "./front-page/FrontPage.js";
-import { Router } from "@reach/router";
-
+import {Route, BrowserRouter} from 'react-router-dom';
 class App extends React.Component {
 
 
     render() {
         return (
-            <Router id="main-container">
-                <FrontPage path="/" />
-                <AdminPanel path="/admin"/>
-            </Router>
+            <div>
+                <Route exact path="/" component={FrontPage}/>
+                <Route exact path="/admin"component={AdminPanel}/>
+            </div>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root')); 
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root')); 
