7feb8ec40989d3252ea0ab753964e70550810404
tomdmitchell <thomasdavymitchell@gmail.com>
2019-08-20 10:29:45 +0100

    fix uploading issue by removing reference to db path when deleting locally

--- back-end/images/remove.js
+++ back-end/images/remove.js
@@ -6,38 +6,21 @@ function remove(req,res) {
     //get the imageId from the checked items when form is posted
     req.setTimeout(0);
     var fileToDelete = req.body;
-    console.log(fileToDelete);
-    //if there is only one entry, call functions.
-    if (typeof fileToDelete === 'string') {
-      console.log('this is a string')
-      deleteLocalPath('photos', {'imagePath': fileToDelete})
-      deleteDbEntry('photos', {'imagePath': fileToDelete});
-     } 
-    else {
-      console.log('this is an array of strings');
-      //if imageId is an array, iterate over each, calling function each time
+    console.log("this is from req.body",req.body[0].imageId);
       fileToDelete.forEach(function(file) {
-        //console.log(file.imageId);
-      deleteLocalPath('photos', {'imagePath': file.imageId})
+      deleteLocalPath(file.imageId);
       deleteDbEntry('photos', {'imagePath': file.imageId});
       })
-    }
+    
   };
   
   //function to delete image locally using path
-  function deleteLocalPath(collectionName, queryParam) {
-    db.collection(collectionName).findOne(queryParam, function(err, result) {
+  function deleteLocalPath(fileToDelete) {
+    fs.unlink(fileToDelete, function(err) {
       if (err) {
-        console.log(err);
+        console.log(err) 
       } else {
-        console.log(result);
-        fs.unlink(result.imagePath, function(err) {
-          if (err) {
-            console.log(err) 
-          } else {
-            console.log('removed locally');
-          }
-        })
+        console.log('removed locally');
       }
     })
   }
