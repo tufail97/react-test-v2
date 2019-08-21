cd64ae9100bbb3536c17fcb5c3581b2e874739e9
tomdmitchell <thomasdavymitchell@gmail.com>
2019-08-21 12:02:42 +0100

    add in placeholder for image info on preview pane

--- src/admin-panel/PreviewRender.js
+++ src/admin-panel/PreviewRender.js
@@ -15,10 +15,18 @@ export default class PreviewRender extends React.Component {
                         {
                             this.props.images.map(function(x,index) {
                                 return (
-                                    <img 
-                                    key={index}
-                                    className="prevImage"
-                                    src={x} />
+                                    <div key={index}>
+                                        <img 
+                                        key={index + "a"}
+                                        className="prevImage"
+                                        src={x} />
+                                        <div key={index+ "b"}>
+                                            <div>XX FILENAME XX</div>
+                                            <div>XX TYPE XX</div>
+                                            <div>XX DIMENSIONS XX</div>
+                                            <div>XX SIZE XX</div>
+                                        </div>
+                                    </div>
                                     )
                                 }
                                 )
