8b84f945ba9acca898feb9d6353f0c659048e93a
tomdmitchell <thomasdavymitchell@gmail.com>
2019-08-28 23:13:00 +0100

    begin adding logout functionality, need to stop underlying elements being clickable

--- src/global-elements/AuthPanel.js
+++ src/global-elements/AuthPanel.js
@@ -3,14 +3,35 @@ import React from "react";
 export default class AuthPanel extends React.Component {
     constructor() {
         super();
+        this.state = {
+            logoutOpacity: 0
+        }
+        this.mouseEnter = this.mouseEnter.bind(this);
+        this.mouseLeave = this.mouseLeave.bind(this);
+    }
+
+    mouseEnter() {
+        console.log('mouse in');
+        this.setState({
+            logoutOpacity: 1
+        })
+        console.log(this.state);
+    }
+
+    mouseLeave() {
+        console.log('mouse out');
+        this.setState({
+            logoutOpacity: 0
+        })
+        console.log(this.state);
     }
 
     render() {
         return(
-            <div className="authPanel">
-                <div>{JSON.parse(localStorage.getItem('currentUser')).username}</div>
-                <div>Admin</div>
-                <button onClick={this.props.logout}>Logout</button>
+            <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className="authPanel">
+                <div className="userDisplay">{JSON.parse(localStorage.getItem('currentUser')).username}</div>
+                <div className="adminDisplay">Admin</div>
+                <button style={{opacity:this.state.logoutOpacity}} onClick={this.props.logout}>Logout</button>
             </div>
         )
     }
