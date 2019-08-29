1b5f65fdf085b901a1e7e45c63bbb830656d733e
tomdmitchell <thomasdavymitchell@gmail.com>
2019-08-29 22:49:13 +0100

    began adding in beginning of touch screen usability

--- src/global-elements/AuthPanel.js
+++ src/global-elements/AuthPanel.js
@@ -4,34 +4,47 @@ export default class AuthPanel extends React.Component {
     constructor() {
         super();
         this.state = {
-            logoutOpacity: 0
+            logoutOpacity: 0,
+            logoutDisplay: "none"
         }
         this.mouseEnter = this.mouseEnter.bind(this);
         this.mouseLeave = this.mouseLeave.bind(this);
+        this.mouseClick = this.mouseClick.bind(this);
     }
 
     mouseEnter() {
         console.log('mouse in');
         this.setState({
-            logoutOpacity: 1
+            logoutOpacity: 1,
+            logoutDisplay: "block"
         })
-        console.log(this.state);
     }
 
     mouseLeave() {
         console.log('mouse out');
         this.setState({
-            logoutOpacity: 0
+            logoutOpacity: 0,
+            logoutDisplay: "none"
         })
-        console.log(this.state);
+    }
+
+    mouseClick() {
+        console.log('clicked');
+        this.setState({
+            logoutDisplay: "block",
+            logoutOpacity: 1
+        })
     }
 
     render() {
+        console.log(this.state);
         return(
             <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} className="authPanel">
-                <div className="userDisplay">{JSON.parse(localStorage.getItem('currentUser')).username}</div>
-                <div className="adminDisplay">Admin</div>
-                <button style={{opacity:this.state.logoutOpacity}} onClick={this.props.logout}>Logout</button>
+                <div onClick={this.mouseClick}>
+                    <div className="userDisplay">{JSON.parse(localStorage.getItem('currentUser')).username}</div>
+                    <div className="adminDisplay">Admin</div>
+                </div>
+                <button style={{opacity:this.state.logoutOpacity, display:this.state.logoutDisplay}} onClick={this.props.logout}>Logout</button>
             </div>
         )
     }
