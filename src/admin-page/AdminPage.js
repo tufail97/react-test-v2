e7a114e414e3e38a3e167b3504a5057450ee35d1
tomdmitchell <thomasdavymitchell@gmail.com>
2019-09-07 17:09:12 +0100

    moved removeChecked function out of main AdminPage component

--- src/admin-page/AdminPage.js
+++ src/admin-page/AdminPage.js
@@ -23,11 +23,11 @@ export default class AdminPage extends React.Component {
             selectedFile: null,
             previewImages: null
         }
-        this.removeChecked = this.removeChecked.bind(this);
         this.onChangeHandler = this.onChangeHandler.bind(this);
         this.getPreviewImages = this.getPreviewImages.bind(this);
         this.getEncodedImage = this.getEncodedImage.bind(this);
         this.onClickHandler = this.onClickHandler.bind(this);
+        this.changeState = this.changeState.bind(this);
     }
 
     componentDidMount() {
@@ -38,31 +38,12 @@ export default class AdminPage extends React.Component {
 
     //////// Thumbnail Render
 
-    removeChecked(valArray) {
-        var headerInfo = {
-            headers: {'Authorization': "bearer " + this.state.currentUser.token}
-        }
-        this.setState({isLoading: true});
-        console.log(this.state.data);
-        Axios.post("http://localhost:3000/images/remove", valArray, headerInfo)
-        .then(res => {
+    changeState(state, newVal) {
+        console.log('in changeState');
+        this.setState({
+            [state]: newVal
         })
-        .catch(function (error) {
-          console.log(error);
-        });
-        var stateCopy = this.state.data;
-        console.log('this is statecopy', stateCopy);
-        for (var j = 0; j < valArray.length; j++) {
-            for (var i = 0; i < stateCopy.length; i++) {
-                if (stateCopy[i]._id === valArray[j].objectId) {
-                    console.log(stateCopy[i]._id, valArray[j].objectId)
-                    stateCopy.splice(i,1);
-                    break;
-                }
-            }
-        }
-        this.setState({data: stateCopy, isLoading: false});
-        console.log(this.state.data);
+        console.log(this.state);
     }
 
     ////////// upload
@@ -92,7 +73,6 @@ export default class AdminPage extends React.Component {
         }
         console.log(this.props.currentUser);
         if (this.state.selectedFile) {
-            var {data, isLoading} = this.state;
             this.setState({isLoading: true});
             console.log(this.state.data);
             var formData = new FormData();
@@ -171,9 +151,9 @@ export default class AdminPage extends React.Component {
 
     displayDecision(type) {
         return type === "image" ? <ThumbnailRender 
+        passUpState={this.changeState}
         data={this.state.data} 
         isLoading={this.state.isLoading} 
-        remove={this.removeChecked}
         currentUser={this.state.currentUser} 
         users={this.state.users}/> 
         : type === "video" ? <AdminPanelVideo />
--- src/admin-page/content/ThumbnailRender.js
+++ src/admin-page/content/ThumbnailRender.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ElementDeleteButton from "./ElementDeleteButton.js";
-
+import Axios from 'axios';
 
 export default class ThumbnailRender extends React.Component {
     constructor() {
@@ -9,6 +9,8 @@ export default class ThumbnailRender extends React.Component {
             checkBoxes: null
         }
         this.getChecked = this.getChecked.bind(this);
+        this.handleState = this.handleState.bind(this);
+        this.removeChecked = this.removeChecked.bind(this);
     }
 
     getChecked() {
@@ -23,7 +25,38 @@ export default class ThumbnailRender extends React.Component {
           }
         })
         this.setState({checkBoxes: checkedVals})
-        this.props.remove(checkedVals);
+        this.removeChecked(checkedVals);
+    }
+
+    removeChecked(valArray) {
+        var headerInfo = {
+            headers: {'Authorization': "bearer " + this.props.currentUser.token}
+        }
+        this.handleState('isLoading', true);
+        Axios.post("http://localhost:3000/images/remove", valArray, headerInfo)
+        .then(res => {
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        var stateCopy = this.props.data;
+        console.log('this is statecopy', stateCopy);
+        for (var j = 0; j < valArray.length; j++) {
+            for (var i = 0; i < stateCopy.length; i++) {
+                if (stateCopy[i]._id === valArray[j].objectId) {
+                    console.log(stateCopy[i]._id, valArray[j].objectId)
+                    stateCopy.splice(i,1);
+                    break;
+                }
+            }
+        }
+        this.handleState('data', stateCopy);
+        this.handleState('isLoading', false);
+        console.log(this.props.data);
+    }
+
+    handleState(state, value) {
+        this.props.passUpState(state, value);
     }
 
     render() {
